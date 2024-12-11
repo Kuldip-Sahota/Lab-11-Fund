1ace27e6ee1883be3c5ea6190e0cf4f63270e698
Kuldip Sahota <kuldipsahota10@gmail.com>
2024-12-11 17:10:52 -0500

    Tested ProductProperties class functionality.

--- ProductProperties.js
+++ ProductProperties.js
@@ -31,3 +31,6 @@ class ProductProperties {
         return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
     }
 }
+const product1 = new ProductProperties("Apple", 2.5, 50);
+console.log(product1.toString());
+console.log("Total Value:", product1.getTotalValue());
