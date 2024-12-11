12491c7554966aa86d51af1d4b057b2ddc45ddd8
Kuldip Sahota <kuldipsahota10@gmail.com>
2024-12-11 17:10:31 -0500

    Added toString method for ProductProperties.

--- ProductProperties.js
+++ ProductProperties.js
@@ -16,3 +16,18 @@ class ProductProperties {
         return this.price * this.quantity;
     }
 }
+class ProductProperties {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    getTotalValue() {
+        return this.price * this.quantity;
+    }
+
+    toString() {
+        return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
+    }
+}
