cc4d62d0dd91bdfea6bccd1e67bbf7313c399db2
Kuldip Sahota <kuldipsahota10@gmail.com>
2024-12-11 17:10:09 -0500

    Added getTotalValue method to ProductProperties.

--- ProductProperties.js
+++ ProductProperties.js
@@ -5,3 +5,14 @@ class ProductProperties {
         this.quantity = quantity;
     }
 }
+class ProductProperties {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    getTotalValue() {
+        return this.price * this.quantity;
+    }
+}
